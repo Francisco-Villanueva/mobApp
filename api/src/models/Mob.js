992cb774542f298733ebdc4005c664213674e414
Francisco Villanueva <franciscovillanuevaj99@gmail.com>
2023-01-31 17:37:41 -0300

    fix db

--- api/src/models/Mob.js
+++ api/src/models/Mob.js
@@ -1,28 +1,27 @@
-const {DataTypes} = require('sequelize')
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    // defino el modelo
-    sequelize.define("mob", {
-      name: {
-        type: DataTypes.STRING,
-      },
-      team: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: 'ally'
-      },
-      color: {
-        type: DataTypes.STRING,
-        defaultValue: 'blue',
-      },
-      type: {
-        type: DataTypes.INTEGER,
-        defaultValue: 'zombie'
-      },
-      life:{
-        type: DataTypes.INTEGER,
-        defaultValue: 50,
-    }
-});
-  };
-  
+  // defino el modelo
+  sequelize.define("mob", {
+    name: {
+      type: DataTypes.STRING,
+    },
+    team: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // defaultValue: "ally",
+    },
+    color: {
+      type: DataTypes.STRING,
+      // defaultValue: "blue",
+    },
+    type: {
+      type: DataTypes.INTEGER,
+      // defaultValue: "zombie",
+    },
+    life: {
+      type: DataTypes.INTEGER,
+      defaultValue: 50,
+    },
+  });
+};
