dd2eca4059497f6da2ff6b8462876dc44414d095
Francisco Villanueva <franciscovillanuevaj99@gmail.com>
2023-03-30 10:14:08 -0300

    commit preuba

--- client/src/components/mobsList/Moblist.jsx
+++ client/src/components/mobsList/Moblist.jsx
@@ -175,7 +175,7 @@ export default function MobList({ mobs, models }) {
 
       <div className="scale-in-ver-bottom code-generator-main">
         <button onClick={handleShowModal} className="code-generator">
-          <p>agregar Mob</p>
+          <p>Add Mob</p>
           <FontAwesomeIcon icon={faCode} />
         </button>
       </div>
