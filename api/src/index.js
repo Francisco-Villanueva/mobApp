4cce751299620d8cd624d51b059287e797f315e3
Francisco Villanueva <franciscovillanuevaj99@gmail.com>
2023-04-04 12:05:32 -0300

    codgi ASII

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,7 @@ const server = require("./app");
 const { conn } = require("./db");
 
 conn.sync({ alter: true }).then(() => {
-  server.listen(4000, () => {
-    console.log("%s server runing on port 4000");
+  server.listen(3001, () => {
+    console.log("%s server runing on port 3001");
   });
 });
